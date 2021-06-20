7c2078af8391ae363b5eb2097a9957d58072f6d0
Apenasgabs <gabers357@gmail.com>
2021-06-19 21:40:02 -0300

    hello Word

--- src/Footer.js
+++ src/Footer.js
@@ -1,16 +1,16 @@
-import './footer.css';
+import './Footer.css';
 
-function footer() {
+function Footer() {
   console.log('oi eu to aqui');
   return (
-    <div className="rodape">
+    <div className="footer">
       <section id="credit">
         <p>
           Made with ❤️ by <a href="https://github.com/Apenasgabs">Apenasgabs</a>
         </p>
       </section>
     </div>
-  )
+  );
 }
 
-export default footer
+export default Footer
