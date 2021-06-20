4a28e78dfd60fba6be3bb9fb93ee7ca58025aebd
Apenasgabs <gabers357@gmail.com>
2021-06-20 00:27:32 -0300

    iniciado processso de criacao do servidor

---
+++ client/src/sever/sever.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const axios = require('axios');
+app.use(express.json());
+
+const teste = axios.create({
+  baseURL: 'https://pokeapi.co/api/v2/pokemon'
+})
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Ta rodando `))
+
+app.get('/test', async(req, res)=>{
+  try {
+    const {data} = await teste.get("/245");
+
+    return res.send({palavra: data.abilities[1]})
+  } catch (error) {
+    
+  }
+})
