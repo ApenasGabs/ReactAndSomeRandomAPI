95d9f4e3fa4be7d1632262e3e54fe2b53c63fb6b
ApenasGabs <Gabers357@gmail.com>
2021-06-21 22:11:45 -0300

    ainda nao foi dessa vez mas to quase 🤩

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Footer from './Footer';
+import App from './Components/App';
+import Footer from './Components/Footer';
 
 ReactDOM.render(<App />,document.getElementById('root') )
-
+ReactDOM.render(<App />,document.getElementById('root') )
 ReactDOM.render(<Footer />,document.getElementById('footer'));
